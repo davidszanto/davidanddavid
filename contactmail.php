19f6b58c4f5624a3fb5b9736911e5dcbfcaf11f1
davefej <davefej@gmail.com>
2018-02-05 20:18:00 +0100

    Bug fixes Plus Contuct Us php side

--- contactmail.php
+++ contactmail.php
@@ -1,1 +1,22 @@
 <?php
+
+$subject = 'Weblap - Üzenet';
+$message = "\r\n\r\n".$_GET['name']."\r\n\r\n".$_GET['message']."\r\n".$_GET['mail']."\r\n"."WEBLAP ÜZENET!\r\n";
+$headers = 'From: '.$_GET['mail']."\r\n";
+$headers .= "Content-Type: text/plain;charset=utf-8\r\n";
+
+if(mail("david@davidanddavid.hu",$subject,$message,$headers)){
+
+}else{
+	saveEmail($message);
+}
+
+function saveEmail($msg){
+	$myfile = fopen("offlinemail.txt", "a") or die("Unable to open file!");
+	$txt = date("now")."\r\n".$msg."\r\n\r\n-------------------\r\n\r\n";
+	fwrite($myfile, $txt);
+	fclose($myfile);
+}
+
+return header('Location: thankyou.html');
+?>
